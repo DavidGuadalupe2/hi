e4ac3c5377fba6943cd2fc1925ad0b872b69a37f
DavidGuadalupe2 <116692117+DavidGuadalupe2@users.noreply.github.com>
2022-12-02 17:43:15 -0500

    Add files via upload

---
+++ 1111111.py
@@ -0,0 +1,10 @@
+//Name:  David Guadalupe
+//Email: david.guadalupe73@myhunter.cuny.edu
+//Date:  November 2019
+//My first program in C++
+
+int main()                 //C++ programs all have a main() function
+{
+  cout << "Hello, World!"; //Print "Hello, World!" to the terminal
+  return 0;                //Standard way to end function
+}
